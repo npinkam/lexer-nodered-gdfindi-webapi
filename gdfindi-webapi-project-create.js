f78697a3f140bdceada8f105d36409a36838a8a8
npinkam <n.pinkam@lexer.co.jp>
2020-11-09 10:14:58 +0900

    add create project node

---
+++ gdfindi-webapi-project-create.js
@@ -0,0 +1,75 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var tableify = require('tableify');
+module.exports = function (RED) {
+
+    function gdfindiWebapiProjectCreateNode(config) {
+
+        RED.nodes.createNode(this, config);
+
+        var node = this;
+
+
+        //properties field
+
+        this.name = config.name;
+
+        this.desc = config.desc;
+
+        this.extended = config.extended;
+
+        this.productionProcesses = config.productionProcesses;
+
+        this.layouts = config.layouts;
+
+        this.stationActivities = config.stationActivities;
+
+        this.transportActivities = config.transportActivities;
+
+        this.masters = config.masters;
+
+        this.assets = config.assets;
+
+        this.renderingCondition = config.renderingCondition;
+
+        this.latitude = config.latitude;
+
+        this.longitude = config.longitude;
+
+
+        // add codeBeforeReceivePayload
+
+
+        node.on('input', function (msg) {
+            // add codeWhenReceivePayload
+            // content to be sent to the server
+            var content = {
+                "name": this.name,
+                "desc": this.desc,
+                "extended": this.extended,
+                "productionProcesses": this.productionProcesses,
+                "layouts": this.layouts,
+                "stationActivities": this.stationActivities,
+                "transportationActivities": this.transportActivities,
+                "masters": this.masters,
+                "assets": this.assets,
+                "renderingCondition": this.renderingCondition,
+                "latitude": this.latitude,
+                "longitude": this.longitude
+            };
+
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", "https://precom.gdfindi.pro/api/v1/projects/", false);
+            xhr.setRequestHeader('Authorization', msg.payload.authorization);
+            xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+            xhr.send(JSON.stringify(content));
+            var response = JSON.parse(xhr.responseText);
+            var html = tableify(response);
+            msg.payload = html;
+            node.send(msg);
+        });
+
+    }
+
+    RED.nodes.registerType("gdfindi-webapi-project-create", gdfindiWebapiProjectCreateNode);
+
+}
