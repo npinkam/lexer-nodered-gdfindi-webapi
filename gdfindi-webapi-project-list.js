6189bad029e28042474da6bba8ffcd882aab301d
npinkam <n.pinkam@lexer.co.jp>
2020-11-11 10:47:08 +0900

    add httpout

--- gdfindi-webapi-project-list.js
+++ gdfindi-webapi-project-list.js
@@ -1,6 +1,6 @@
-var tableify = require('tableify');
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 module.exports = function (RED) {
+    var tableify = require('tableify');
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
     function gdfindiWebapiProjectListNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
@@ -8,7 +8,7 @@ module.exports = function (RED) {
         //properties field
 
         // add codeBeforeReceivePayload
-        node.on('input', function (msg) {
+        node.on('input', function (msg, _send, done) {
             // add codeWhenReceivePayload
             var xhr = new XMLHttpRequest();
             xhr.open("GET", "https://precom.gdfindi.pro/api/v1/projects/", false);
@@ -25,7 +25,80 @@ module.exports = function (RED) {
             */
             var html = tableify(response);
             msg.payload = html;
-            node.send(msg);
+
+            /* -------- Test http out -------- */
+
+            this.headers = {};
+            this.statusCode = "";
+
+            if (msg.res) {
+                var headers = RED.util.cloneMessage(node.headers);
+                if (msg.headers) {
+                    if (msg.headers.hasOwnProperty('x-node-red-request-node')) {
+                        var headerHash = msg.headers['x-node-red-request-node'];
+                        delete msg.headers['x-node-red-request-node'];
+                        var hash = hashSum(msg.headers);
+                        if (hash === headerHash) {
+                            delete msg.headers;
+                        }
+                    }
+                    if (msg.headers) {
+                        for (var h in msg.headers) {
+                            if (msg.headers.hasOwnProperty(h) && !headers.hasOwnProperty(h)) {
+                                headers[h] = msg.headers[h];
+                            }
+                        }
+                    }
+                }
+                if (Object.keys(headers).length > 0) {
+                    msg.res._res.set(headers);
+                }
+                if (msg.cookies) {
+                    for (var name in msg.cookies) {
+                        if (msg.cookies.hasOwnProperty(name)) {
+                            if (msg.cookies[name] === null || msg.cookies[name].value === null) {
+                                if (msg.cookies[name] !== null) {
+                                    msg.res._res.clearCookie(name, msg.cookies[name]);
+                                } else {
+                                    msg.res._res.clearCookie(name);
+                                }
+                            } else if (typeof msg.cookies[name] === 'object') {
+                                msg.res._res.cookie(name, msg.cookies[name].value, msg.cookies[name]);
+                            } else {
+                                msg.res._res.cookie(name, msg.cookies[name]);
+                            }
+                        }
+                    }
+                }
+                var statusCode = node.statusCode || msg.statusCode || 200;
+                if (typeof msg.payload == "object" && !Buffer.isBuffer(msg.payload)) {
+                    msg.res._res.status(statusCode).jsonp(msg.payload);
+                } else {
+                    if (msg.res._res.get('content-length') == null) {
+                        var len;
+                        if (msg.payload == null) {
+                            len = 0;
+                        } else if (Buffer.isBuffer(msg.payload)) {
+                            len = msg.payload.length;
+                        } else if (typeof msg.payload == "number") {
+                            len = Buffer.byteLength("" + msg.payload);
+                        } else {
+                            len = Buffer.byteLength(msg.payload);
+                        }
+                        msg.res._res.set('content-length', len);
+                    }
+
+                    if (typeof msg.payload === "number") {
+                        msg.payload = "" + msg.payload;
+                    }
+                    msg.res._res.status(statusCode).send(msg.payload);
+                }
+            } else {
+                node.warn(RED._("httpin.errors.no-response"));
+            }
+            done();
+
+            /* -------- Test http out -------- */
         });
     }
     RED.nodes.registerType("Project: List", gdfindiWebapiProjectListNode);
