4f4f835b746beafb7e91de6ae6c797fe8b2ee684
npinkam <n.pinkam@lexer.co.jp>
2020-11-12 10:25:23 +0900

    pvdo list add htmlOut

--- gdfindi-webapi-pvdo-list.js
+++ gdfindi-webapi-pvdo-list.js
@@ -1,6 +1,8 @@
-var tableify = require('tableify');
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 module.exports = function (RED) {
+    var tableify = require('tableify');
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    var httpOut = require('./lib/httpOut.js');
+
     function gdfindiWebapiPvdoListNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
@@ -8,7 +10,7 @@ module.exports = function (RED) {
         //properties field
 
         // add codeBeforeReceivePayload
-        node.on('input', function (msg) {
+        node.on('input', function (msg, done) {
             // add codeWhenReceivePayload
             var xhr = new XMLHttpRequest();
             xhr.open("GET", "https://precom.gdfindi.pro/api/v1/PVDO", false);
@@ -17,7 +19,9 @@ module.exports = function (RED) {
             var response = JSON.parse(xhr.responseText);
             var html = tableify(response);
             msg.payload = html;
-            node.send(msg);
+            
+            /* -------- http out -------- */
+            httpOut(RED, node, msg, done);
         });
     }
     RED.nodes.registerType("PVDO: List", gdfindiWebapiPvdoListNode);
