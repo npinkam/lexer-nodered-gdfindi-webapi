ff13eed5658860a00ec88fadc3e997944a9417b1
npinkam <n.pinkam@lexer.co.jp>
2020-11-13 15:39:53 +0900

    delete node is done

--- gdfindi-webapi-project-info.js
+++ gdfindi-webapi-project-info.js
@@ -8,7 +8,6 @@ module.exports = function (RED) {
     function gdfindiWebapiProjectInfoNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
-        var enableNode = false;
 
         //properties field
 
@@ -17,44 +16,48 @@ module.exports = function (RED) {
 
         //callback function when url is accessed
         this.callback = function (req, res, done) {
-            if (enableNode == true) {
-                /** mandatory **/
-                var msgid = RED.util.generateId();
-                res._msgid = msgid;
-                /** mandatory **/
+            /** mandatory **/
+            var msgid = RED.util.generateId();
+            res._msgid = msgid;
+            /** mandatory **/
 
-                var projectId = req.query.projectId;
-                var xhr = new XMLHttpRequest();
-                xhr.open("GET", "https://precom.gdfindi.pro/api/v1/projects/" + projectId, false);
-                xhr.setRequestHeader('Authorization', req.cookies.authorization);
-                xhr.send();
-                var response = JSON.parse(xhr.responseText);
-                var html = tableify(response);
+            var projectId = req.query.projectId;
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "https://precom.gdfindi.pro/api/v1/projects/" + projectId, false);
+            xhr.setRequestHeader('Authorization', req.cookies.authorization);
+            xhr.send();
+            var response = JSON.parse(xhr.responseText);
+            var html = tableify(response);
 
-                //node.send structure:
-                //get -> payload: req.query
-                //other -> payload: req.body
-                //node.send({ _msgid: msgid, req: req, res: wrapper.createResponseWrapper(node, res), payload: payload })
-                var msg = { _msgid: msgid, req: req, res: wrapper.createResponseWrapper(node, res), payload: "" };
-                //node.emit('input', msg);
-                msg.payload = html;
+            //node.send structure:
+            //get -> payload: req.query
+            //other -> payload: req.body
+            //node.send({ _msgid: msgid, req: req, res: wrapper.createResponseWrapper(node, res), payload: payload })
+            var msg = { _msgid: msgid, req: req, res: wrapper.createResponseWrapper(node, res), payload: "" };
+            //node.emit('input', msg);
+            msg.payload = html;
 
-                // -------- http out -------- 
-                httpOut(RED, node, msg, done);
+            // -------- http out -------- 
+            httpOut(RED, node, msg, done);
 
-                // -------- send raw data
-                msg.payload = {};
-                msg.payload = response;
-                node.send(msg);
-            }
+            // -------- send raw data
+            msg.payload = {};
+            msg.payload = response;
+            node.send(msg);
+
         }
-
-        // call httpInput library
-        new httpIn(RED, node, this.url, this.method, this.callback);
 
         // add codeBeforeReceivePayload
         node.on('input', function (msg) {
-            enableNode = true;
+            //close connection
+            var node = this;
+            RED.httpNode._router.stack.forEach(function (route, i, routes) {
+                if (route.route && route.route.path === node.url && route.route.methods[node.method]) {
+                    routes.splice(i, 1);
+                }
+            });
+            // call httpInput library
+            new httpIn(RED, node, this.url, this.method, this.callback);
         });
     }
     RED.nodes.registerType("Project: Information", gdfindiWebapiProjectInfoNode);
