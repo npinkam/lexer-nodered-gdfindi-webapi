8aeebecf086716c6a5870648499a570e5cdbb945
npinkam <n.pinkam@lexer.co.jp>
2020-11-16 14:34:28 +0900

    edit pvdo list

--- gdfindi-webapi-pvdo-list.js
+++ gdfindi-webapi-pvdo-list.js
@@ -1,13 +1,37 @@
 module.exports = function (RED) {
-    var tableify = require('tableify');
-    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    var httpOut = require('./lib/httpOut.js');
+    const tableify = require('tableify');
+    const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    const httpOut = require('./lib/httpOut.js');
+    const httpIn = require('./lib/httpIn.js');
+    const wrapper = require('./lib/wrapper.js');
+    
 
     function gdfindiWebapiPvdoListNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
 
-        //properties field
+        this.url = "/PVDOreq";
+        this.method = "get";
+
+        //callback function when url is accessed
+        this.callback = function (req, res, done) {
+            /** mandatory **/
+            var msgid = RED.util.generateId();
+            res._msgid = msgid;
+            /** mandatory **/
+
+            //node.send structure:
+            //get -> payload: req.query
+            //other -> payload: req.body
+            //node.send({ _msgid: msgid, req: req, res: wrapper.createResponseWrapper(node, res), payload: payload })
+            var msg = { _msgid: msgid, req: req, res: wrapper.createResponseWrapper(node, res), payload: req.query };
+            // -------- http out -------- 
+            httpOut(RED, node, msg, done);
+            node.send(msg);
+
+        }
+
+        httpIn(RED, node, this.url, this.method, this.callback);
 
         // add codeBeforeReceivePayload
         node.on('input', function (msg, done) {
@@ -17,10 +41,22 @@ module.exports = function (RED) {
             xhr.setRequestHeader('Authorization', msg.cookies.authorization);
             xhr.send();
             var response = JSON.parse(xhr.responseText);
+
+            //current mining
+            var buffer = response.miningmanager_status.CurrentMiningID;
+            var link = `/PVDOreq?MiningID=${buffer}`;
+            response.miningmanager_status.CurrentMiningID = "<a href=" + link + " target='_self'>" + buffer + "</a>"; 
+            
+            response.mining_statuses.forEach(element => {
+                var buffer = element.MiningID;
+                var link = `/PVDOreq?MiningID=${buffer}`;
+                element.MiningID = "<a href=" + link + " target='_self'>" + buffer + "</a>";
+            });
+
             var html = tableify(response);
             msg.payload = html;
-            
-            /* -------- http out -------- */
+
+           // -------- http out -------- 
             httpOut(RED, node, msg, done);
         });
     }
