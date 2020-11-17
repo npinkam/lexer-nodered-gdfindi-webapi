c1e0984fa0e4a1ab9e64bfaf2521421396d7798e
npinkam <n.pinkam@lexer.co.jp>
2020-11-17 16:39:46 +0900

    fix project list: /req on project list now

--- gdfindi-webapi-project-list.js
+++ gdfindi-webapi-project-list.js
@@ -1,15 +1,39 @@
 module.exports = function (RED) {
-    var tableify = require('tableify');
-    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    var httpOut = require('./lib/httpOut.js');
+    "use strict";
+    const tableify = require('tableify');
+    const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    const httpIn = require('./lib/httpIn.js');
+    const httpOut = require('./lib/httpOut.js');
+    const wrapper = require('./lib/wrapper.js');
 
     function gdfindiWebapiProjectListNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
 
         //properties field
+        this.enableCreate = config.enableCreate;
 
-        // add codeBeforeReceivePayload
+        this.url = "/req";
+        this.method = "get";
+
+        this.msgContent = {};
+
+        //callback function when url is accessed
+        this.callback = function (req, res) {
+            /** mandatory **/
+            var msgid = RED.util.generateId();
+            res._msgid = msgid;
+            /** mandatory **/
+            var projectId = req.query.projectId;
+            var msg = { _msgid: msgid, req: req, res: wrapper.createResponseWrapper(node, res), payload: {} };
+
+            // -------- send raw data
+            msg.payload = projectId;
+            node.send(msg);
+        }
+
+        httpIn(RED, node, this.url, this.method, this.callback);
+
         node.on('input', function (msg, done) {
             // add codeWhenReceivePayload
             var xhr = new XMLHttpRequest();
@@ -17,10 +41,7 @@ module.exports = function (RED) {
             xhr.setRequestHeader('Authorization', msg.req.cookies.authorization);
             xhr.send();
             var response = JSON.parse(xhr.responseText);
-            msg.payload = {};
-            msg.payload = response;
-            node.send(msg);
-            
+
             response.forEach(element => {
                 var buffer = element.id;
                 var link = `/req?projectId=${buffer}`;
