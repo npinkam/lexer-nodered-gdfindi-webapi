9ae0a218fb5e17f9278268a9ea8e8d4f8755c38a
npinkam <n.pinkam@lexer.co.jp>
2020-12-18 17:22:57 +0900

    add color palette

--- gdfindi-webapi-pvdo-workload-timeline.js
+++ gdfindi-webapi-pvdo-workload-timeline.js
@@ -254,7 +254,13 @@ module.exports = function (RED) {
                 timeline: { colorByRowLabel: true },
                 labelStyle: {
                         fontSize: 13
-                } 
+                } ,
+                colors: ['#111e6c','#1d2951', '#597387', '#003152', 
+                '#00080', '#0e4d92', '#1034a6', '#0080ff', '#0f52ba', 
+                '#008ecc', '#6593f5', '#4c516d', '#008081', '#73c2fb', 
+                '#7285a5', '#4f97a3', '#57a0d3', '#4682b4', '#81d8d0', 
+                '#89cff0', '#588bae', '#7ef9ff', '#95c8d8', '#b0dfe5', 
+                '#3fe0d0']
             };
           
               chart.draw(dataTable, options);
