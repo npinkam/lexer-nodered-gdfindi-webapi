c4cf27c2b4b4ab3d6160d3afc98763bce819eb10
npinkam <n.pinkam@lexer.co.jp>
2020-12-18 17:26:13 +0900

    fix loader position

--- gdfindi-webapi-project-info.js
+++ gdfindi-webapi-project-info.js
@@ -443,7 +443,7 @@ module.exports = function (RED) {
         }
         .loader {
           position: absolute;
-          left: 47%;
+          left: 46.5%;
           top: 48%;
           z-index: 1;
           border: 16px solid #f3f3f3;
