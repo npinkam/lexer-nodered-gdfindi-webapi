ee2004a45e402b2d2fc29d030ed47d2e0268c5b1
npinkam <n.pinkam@lexer.co.jp>
2020-12-24 16:06:41 +0900

    misc

--- gdfindi-webapi-project-info.js
+++ gdfindi-webapi-project-info.js
@@ -382,7 +382,7 @@ module.exports = function (RED) {
         if (hasRenderingCondition == true) {
           response.renderingCondition.productionSchedules[0].orders.forEach(element => {
               process.push({
-                "productid": response.productionProcesses[0].name, //name of process
+                "productid": element.name, //name of process
                 "lotsize": element.lotsize, // lot size
                 "daytime": null, // Math.floor(Math.random() * 86400), //start time
                 "islot": false, //  Lot
