6d3de3d5807bfd5cd1e1f187b8b6bb8a4e845613
npinkam <n.pinkam@lexer.co.jp>
2021-01-27 16:29:06 +0900

    test headers

--- gdfindi-webapi-pvdo-workload-timeline.js
+++ gdfindi-webapi-pvdo-workload-timeline.js
@@ -321,7 +321,7 @@ module.exports = function (RED) {
             $.ajax({
               method: "POST",
               url: "http://10.3.4.30:8083/rest/data",
-              headers: {'x-requested-with': 'application/json'},
+              headers: {'x-requested-with': '"Content-Type": "application/json"'},
               data: ${dataText},
               success: (result)=>{
                 if (result.error == "true") 
